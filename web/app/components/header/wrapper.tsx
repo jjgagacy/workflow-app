943c75b0a1d80de5db7f9ab71a8a753695316914
alex <jjgagacy@163.com>
2026-02-01 15:20:34 +0800

    dark theme adjust

--- web/app/components/header/wrapper.tsx
+++ web/app/components/header/wrapper.tsx
@@ -1,6 +1,6 @@
 export default function HeaderWrapper({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col bg-white dark:bg-neutral-800">
+    <div className="flex flex-col bg-muted-light">
       {children}
     </div>
   );
